a5302a78183eec1ff470c798fe8fe4e25688025d
Enrique Carral Trevino <ecarralt@gmail.com>
2016-07-04 18:45:12 -0500

    finished styling

--- test/features/user_can_sign_up_test.rb
+++ test/features/user_can_sign_up_test.rb
@@ -11,7 +11,7 @@ class UserCanSignUpTest < Capybara::Rails::TestCase
     fill_in('Last name', :with => "carral")
     fill_in('Username', :with => "ecarralt")
     fill_in('Password', :with => "12345678")
-    fill_in('Password confirmation', :with => "12345678")
+    fill_in('Re-type password', :with => "12345678")
     click_button "Submit"
     assert_content page, "Start sharing your thoughts and follow your favorite Thinkers!"
 
